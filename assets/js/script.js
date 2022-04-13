1ad66a8abd57e62f096bb56497f7f3948f4e937a
Cozmin-Ghenea <43807754+Cozmin-Ghenea@users.noreply.github.com>
2022-04-13 19:06:17 +0300

    Finished the masterpiece

--- assets/js/script.js
+++ assets/js/script.js
@@ -107,7 +107,7 @@ modalCloses.forEach((modalClose) =>{
 
 // ======================== Portofoli Swiper ====================
 
-let swiper = new Swiper('.portofolio__container', {
+let swiperPortofolio = new Swiper('.portofolio__container', {
     cssMode: true,
     loop:true,
     
@@ -122,9 +122,104 @@ let swiper = new Swiper('.portofolio__container', {
 })
 
 // ===================== Testimonials ==================
+let swiperTestimonial = new Swiper('.testimonial__container', {
+    loop:true,
+    grabCursor : true,
+    spaceBetween:48,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable:true,
+        dynamicBullets:true,
+    },
+
+    breakpoints:{
+        568:{
+            slidesPerView:2,
+        }
+    }
+})
 
 
 // ===================== Scroll Sections Active Link ==================
 
 
+const sections =  document.querySelectorAll('section[id]')
+
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current. offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight ){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
 // ===================== Change backgrkound header==================
+
+
+function scrollHeader(){
+    const nav = document.getElementById('header')
+
+    if(this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header');
+}
+
+window.addEventListener('scroll', scrollHeader)
+
+
+// ===================================== Show Scroll Top ================
+
+function scrolltop(){
+    const scrolltop = document.getElementById('scroll-up')
+
+    if(this.scrollY >= 560) scrolltop.classList.add('show-scroll'); else scrolltop.classList.remove('show-scroll');
+}
+
+window.addEventListener('scroll', scrolltop)
+
+// ======================================= Dark Light Theme =========================================
+
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'fa-sun'
+
+//Previously selection
+
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// Obtain the current theme that interface has by comparing with the dark theme class 
+
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'fa-moon' : 'fa-sun'
+
+// Checking the preovius preference
+
+if(selectedTheme){
+    // If the validation is fullfilled, we ask what the issuewas to know if we activate or deactivate the dark theme
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedIcon === 'fa-moon' ? 'add' : 'remove'](iconTheme)
+}
+//Activate / dactivate he theme manuall with the button
+
+themeButton.addEventListener('click', () =>{
+    //Add remove dark icone and theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+
+    // Save the theme and the current icon that the user choose
+    localStorage.setItem('selected-theme',getCurrentTheme())
+    localStorage.setItem('selected-icon',getCurrentTheme())
+    
+})
+
+
+
